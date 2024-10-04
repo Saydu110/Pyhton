99c0e3e8adffd53ce008aea26178e28f4d3e5a1e
Saydullo_0208 <156135658+Saydu110@users.noreply.github.com>
2024-10-04 16:21:59 +0500

    update

---
+++ PythonLesson_1/main.py
@@ -0,0 +1,15 @@
+# n = input("n : ")
+# if n.isdigit():
+#     print(True)
+# else :
+#     print("Siz son kiritmadingiz!!!!!!!!!!!!!!!!!!!!!!")
+
+# num = {21,2345,67,"afjsfe"}
+# for i in num[0:3:2]:
+#     print(i)
+
+
+a = [x for x in range(10) if x%2==0] 
+print(a)
+
+
